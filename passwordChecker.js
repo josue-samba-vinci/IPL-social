ea195bab841a75b8c4b3d1c0499147341c89ebac
Josué Samba <josue.sambadianzungu@student.vinci.be>
2025-01-17 16:32:26 +0100

    add : number verification method with test

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,9 +1,12 @@
 function validatePassword(password) {
     const minLength = 8;
     const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+    const digitRegex = /\d/;
 
     if (password.length < minLength) return false;
     if (!specialCharRegex.test(password)) return false;
+    if (!digitRegex.test(password)) return false;
+    
 
     return true;
 
--- passwordChecker.test.js
+++ passwordChecker.test.js
@@ -2,7 +2,7 @@ const validatePassword = require('./passwordChecker');
 
 describe('Password Validation', () => {
 
-    
+
   test('should fail if password is less than 8 characters', () => {
     expect(validatePassword('abc123!')).toBe(false);
   });
@@ -11,5 +11,11 @@ describe('Password Validation', () => {
   test('should fail if password does not contain a special character', () => {
     expect(validatePassword('abc12345')).toBe(false);
   });
+
+
+  test('should fail if password does not contain a digit', () => {
+    expect(validatePassword('abcdefg!')).toBe(false);
+  });
+  
 
 });
