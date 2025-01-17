d4a476a0d58b7ffafd5055c3bb847c70e887c816
Josué Samba <josue.sambadianzungu@student.vinci.be>
2025-01-17 16:29:24 +0100

    add : special character verification method with test

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,7 +1,9 @@
 function validatePassword(password) {
     const minLength = 8;
+    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
 
     if (password.length < minLength) return false;
+    if (!specialCharRegex.test(password)) return false;
 
     return true;
 
--- passwordChecker.test.js
+++ passwordChecker.test.js
@@ -1,8 +1,15 @@
 const validatePassword = require('./passwordChecker');
 
 describe('Password Validation', () => {
+
+    
   test('should fail if password is less than 8 characters', () => {
     expect(validatePassword('abc123!')).toBe(false);
+  });
+
+
+  test('should fail if password does not contain a special character', () => {
+    expect(validatePassword('abc12345')).toBe(false);
   });
 
 });
